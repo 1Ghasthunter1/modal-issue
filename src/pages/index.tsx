b5095e235800168cdb67623aa16d66a7a12fc565
Hunter Pruett <hunterpruett2003@gmail.com>
2023-06-02 17:57:05 +0000

    added text padding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const text =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac purus nec ligula tincidunt semper at quis leo. Ut varius pharetra erat at aliquet. Quisque dictum, lorem ut rhoncus ultrices, magna sapien consequat dolor, non ornare lacus nibh interdum ante. In consequat imperdiet consequat. Nullam tempor ante nec lorem efficitur, eget tincidunt risus condimentum. Vivamus dolor velit, convallis vestibulum lacinia nec, fringilla vel enim. Nulla viverra accumsan molestie. Morbi vel molestie sapien. Aenean tincidunt, mi vel eleifend finibus, sapien neque rhoncus velit, non ultricies lectus sem ac mi.";
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen px-8">
       <div className="z-10 w-full max-w-5xl mt-24 space-y-24">
         {new Array(10).fill(0).map((_, i) => (
           <p key={i}>{text}</p>
